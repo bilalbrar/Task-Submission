cbe88aab39eb666f7dd3ac16939e8e2f7d7b3dff
bilalbrar <bilalbrar5@gmail.com>
2025-04-01 00:30:12 +0100

    Task Submission

--- app/core/metrics.py
+++ app/core/metrics.py
@@ -4,5 +4,5 @@ REQUEST_LATENCY = Histogram(
     'http_request_latency_seconds',
     'Request latency in seconds',
     ['method', 'endpoint'],
-    buckets=[0.001, 0.0025, 0.005, 0.01, 0.025, 0.05, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0]
+    buckets=[0.001, 0.0025, 0.005, 0.01, 0.025, 0.05, 0.1, 0.2, 0.3, 0.5, 1.0, 2.0, 5.0]
 )
